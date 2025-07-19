6acbb3f9e82564f67995524e7d13218553a6dc1c
alexisserrato <alexisserrato1992@gmail.com>
2025-07-18 22:27:20 -0500

    modificacion de codigo crear un new user

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const Usuario = require("../models/Usuarios");
 
 exports.obtenerUsuarios= async(req, res)=>{
@@ -33,23 +34,50 @@ exports.obtenerUsuariosPorId=async(req,res)=>{
 }
 }
 
-
-
 exports.crearNuevoUsuario = async (req, res) => {
-    const { nombre, email } = req.body;
-    const existeUsuario = await Usuario.findOne({ email:email } );
-    console.log("res",existeUsuario)
-    if (existeUsuario) {
-        return res.status(409).json({ 
-            mensaje: "El email ya está registrado" 
+    try {
+        const { nombre, email, password } = req.body;
+        if (!nombre , !email , !password) {
+            return res.status(400).json({ mensaje: "Nombre, email y password son obligatorios" });
+        }
+        const existeUsuario = await Usuario.findOne({ where: { email } });
+        if (existeUsuario) {
+            return res.status(409).json({ 
+                mensaje: "El email ya está registrado" 
+            });
+        }
+        const nuevoUsuario = await Usuario.create({
+            nombre,
+            email,
+            password
         });
+
+        return res.status(201).json({
+            mensaje: "Usuario creado correctamente",
+            usuarioCreado: nuevoUsuario
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ mensaje: "Error interno del servidor" });
     }
-    const nuevoUsuario = await Usuario.create({ nombre, email, password }); // Crear nuevo usuario
-    res.status(201).json({                            
-        mensaje: "Usuario creado correctamente", // Respuesta exitosa
-        usuarioCreado: nuevoUsuario
-    });
 };
+
+// exports.crearNuevoUsuario = async (req, res) => {
+//     const { nombre, email } = req.body;
+//     const existeUsuario = await Usuario.findOne({where:{ email:email } });
+//     console.log("res",existeUsuario)
+//     if (existeUsuario) {
+//         return res.status(409).json({ 
+//             mensaje: "El email ya está registrado" 
+//         });
+//     }
+//     const nuevoUsuario = await Usuario.create({ nombre, email, password }); // Crear nuevo usuario
+//     res.status(201).json({                            
+//         mensaje: "Usuario creado correctamente", // Respuesta exitosa
+//         usuarioCreado: nuevoUsuario
+//     });
+// };
 
 // // crear un nuevo usuario
 // app.post("/usuarios/crear",(req, res)=>{
